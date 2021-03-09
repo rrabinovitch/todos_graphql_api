6a546da932a37ce02641f02ddf558ef9f5bcf680
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2021-03-08 18:53:29 -0700

    setup todo_list test

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:todo_list)).to be_valid
+  end
+
+  let(:attributes) do
+    {
+      title: 'A test title'
+    }
+  end
+
+  let(:todo_list) { create(:todo_list, **attributes) }
+
+  describe 'model validations' do
+    it { expect(todo_list). to allow_value(attributes[:title]).for(:title) }
+    it { expect(todo_list). to validate_presence_of(:title) }
+    it { expect(todo_list).to validate_uniqueness_of(:title) }
+  end
+
+  describe 'model associations' do
+    it { expect(todo_list).to have_many(:items)}
+  end
 end
